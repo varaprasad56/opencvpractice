688c250eb4a089a721b0ca0b3fffa184eb7146f0
varaprasad56 <varaprasad.512@gmail.com>
2020-06-17 00:00:14 +0530

    added video saving code

--- day2.py
+++ day2.py
@@ -1,4 +1,18 @@
 import cv2
 import matplotlib.pyplot as plt
-import numpy
+import numpy as np
 import pandas
+cam = cv2.VideoCapture(0)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('cvexperiment.avi', fourcc, 20, (640, 480))
+while True:
+    feedExists, frame = cam.read()
+    greyImg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('Unchanged', frame)
+    cv2.imshow('Modified', greyImg)
+    out.write(frame)
+    if(cv2.waitKey(1) == ord('q')):
+        break
+out.release()
+cam.release()
+cv2.destroyAllWindows()
